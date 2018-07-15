d844b9ed51629f981854bce66f3151a1c9570d35
gusarov2k <motoface@yandex.ru>
2018-07-15 15:07:26 +0300

    change model and migration

--- db/migrate/20180715114939_create_tasks.rb
+++ db/migrate/20180715114939_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.references :assignee, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.belongs_to :project, index: true, foreign_key: true
       t.datetime :due_date
       t.string :name
