ee17b2631cb035b96a5c8453c10cffac8fd7c51b
gusarov2k <motoface@yandex.ru>
2018-07-16 19:47:28 +0300

    add test for show

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -34,6 +34,31 @@ require 'rails_helper'
 			end
 
 		end
+
+		context "when user logged id" do
+			let(:user) {FactoryBot.create(:user)}
+			subject {FactoryBot.create(:project, owner: user)}
+
+			before do
+				sing_in user
+			end
+
+			describe "GET #show" do
+				it "assigns the requested project to subject" do
+					get :show, id: subject
+					expect(assigns(:project)).to eq(subject)
+				end
+
+				it "renders the :show view" do
+					get :show, id: subject
+					expect(response).to render_template :show
+				end
+			end
+		end
+
 	end
 
-end
+
+
+
+
