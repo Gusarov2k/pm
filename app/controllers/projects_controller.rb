4b8f8194d630694e192146a276c8fd265af7af41
gusarov2k <motoface@yandex.ru>
2018-07-16 19:30:51 +0300

    add rspect tests

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,6 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.includes(:owner)
   end
+
+  
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe ProjectsController, type: :controller do
+# RSpec.describe ProjectsController, type: :controller do
+
+	describe ProjectsController do
+		context "when user not logged in" do
+			describe "GET #index" do
+				it "redirect to login page" do
+					get :index
+					expect(response).to redirect_to new_user_session_path
+				end
+			end
+		end
+
+		context "when user logged in" do
+			let(:user) {FactoryBot.create(:user)}
+			subject {FactoryBot.create(:project, owner: user)}
+
+			before do
+				sing_in user
+			end
+
+			describe "GET #index" do
+				it "render :index view" do
+					get :index
+					expect(response).to render_template :index
+				end
+
+				it "assigns the requested project to subject" do
+					get :index
+					expect(assigns(:project)).to eq([subject])
+				end
+
+			end
+
+		end
+	end
 
 end
