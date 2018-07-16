63418e7508e8c6dee425e19caec63ee48ac791af
gusarov2k <motoface@yandex.ru>
2018-07-16 20:09:16 +0300

    add method creat

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,22 @@ class ProjectsController < ApplicationController
 
   def show
   	@project = Project.find(params[:id])
-
   end
+
+  def new
+  	@project = Project.new
+  end
+
+  def create
+  	@project = current_user.project.new(params.require(:project).permit(:name))
+  	
+  	if @project.save
+  		redirect_to project_path
+  	else
+  		render :new
+  	end
+  	
+  end
+
 
 end
