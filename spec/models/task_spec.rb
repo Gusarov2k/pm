1dc597d213bdd33b223c2329f6e776b2a7a6299d
gusarov2k <motoface@yandex.ru>
2018-07-19 22:55:45 +0300

    add test validates

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:task)).to be_valid
+  end
+
+  it {is_expected.to validate_presence_of(:name)}
+  it {is_expected.to validate_presence_of(:description)}
 end
