53c3630bafd41a0404c495eb1df6499dbea4fba4
gusarov2k <motoface@yandex.ru>
2018-07-19 22:47:54 +0300

    add tests validates for model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
 RSpec.describe Project do
 	it "has a valid factory" do
-		expect(FactoryBot.build(:project)).to be_valid
-	end
-	# it { is_expected belong_to(:user).class_name(User) }
+    expect(FactoryBot.build(:project)).to be_valid
+  end
+
+  it {is_expected.to belongs_to(:user)}
+  it {is_expected.to has_many(:tasks)}
+  it {is_expected.to ensure_length_of(:name).is_at_least(4).is_at_most(30)}
+  it {is_expected.not_to allow_value('').for(:name)}
+  it {is_expected.to validate_presence_of(:user)}
 end
