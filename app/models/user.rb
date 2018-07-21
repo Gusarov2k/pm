efb5a409162f6e7ef1c0db2d9f703ec2305a420d
gusarov2k <motoface@yandex.ru>
2018-07-21 14:31:53 +0300

    change links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :tasks, foreign_key: 'assignee_id'
+  has_many :tasks, foreign_key: 'user_id'
+  has_many :tasks, foreign_key: 'project_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
