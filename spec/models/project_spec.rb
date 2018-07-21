010f79d5756430e5717fce08d65fcf3e6989d169
gusarov2k <motoface@yandex.ru>
2018-07-21 14:50:46 +0300

    change sholda mathcers find errors

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Project do
     expect(FactoryBot.build(:project)).to be_valid
   end
 
-  it {is_expected.to belongs_to(:user)}
-  it {is_expected.to has_many(:tasks)}
-  it {is_expected.to ensure_length_of(:name).is_at_least(4).is_at_most(30)}
+  it {is_expected.to belong_to(:user)}
+  it {is_expected.to have_many(:tasks)}
+  it {is_expected.to validate_length_of(:name).is_at_least(4).is_at_most(30)}
   it {is_expected.not_to allow_value('').for(:name)}
   it {is_expected.to validate_presence_of(:user)}
 end
